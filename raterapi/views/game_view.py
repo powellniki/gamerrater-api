bb6dbfa7904e700a4a91c5e71887765ec4bf86b6
Niki Powell <nhe.powell@gmail.com>
2024-06-21 14:27:09 -0500

    updated logic for IS OWNER for updating game

--- raterapi/views/game_view.py
+++ raterapi/views/game_view.py
@@ -9,7 +9,7 @@ class GameView(ViewSet):
     def retrieve(self, request, pk=None):
         try:
             game = Game.objects.get(pk=pk)
-            serialized = GameSerializer(game, many=False)
+            serialized = GameSerializer(game, many=False, context={'request': request})
             return Response(serialized.data, status=status.HTTP_200_OK)
 
         except Game.DoesNotExist:
@@ -18,7 +18,7 @@ class GameView(ViewSet):
 
     def list(self, request):
         games = Game.objects.all()
-        serialized = GameSerializer(games, many=True)
+        serialized = GameSerializer(games, many=True, context={'request': request})
         return Response(serialized.data, status=status.HTTP_200_OK)
 
 
@@ -55,37 +55,31 @@ class GameView(ViewSet):
         return Response(serialized_game.data, status=status.HTTP_201_CREATED)
 
 
-    def update(self, request, pk):
-        # extract fields from client request body
-        title = request.data.get('title', None)
-        description = request.data.get('description', None)
-        designer = request.data.get('designer', None)
-        release_year = request.data.get('release_year', None)
-        number_players = request.data.get('number_players', None)
-        play_time = request.data.get('play_time', None)
-        age_rec = request.data.get('age_rec', None)
-        # user=request.user
-
-        if title is None or description is None or designer is None or release_year is None or number_players is None or play_time is None or age_rec is None:
-            return Response({'message': 'You must provide all properties in your JSON'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Update existing row in database
+    def update(self, request, pk=None):
         try:
-            requested_game = Game.objects.get(pk=pk)
-            requested_game.title = title
-            requested_game.description = description
-            requested_game.designer = designer
-            requested_game.release_year = release_year
-            requested_game.number_players = number_players
-            requested_game.play_time = play_time
-            requested_game.age_rec = age_rec
-            requested_game.save()
+            game = Game.objects.get(pk=pk)
 
-            # Send data back in a response, with 204 status code
-            return Response(None, status=status.HTTP_204_NO_CONTENT)
+            # is the authenticated user allowed to edit the game?
+            self.check_object_permissions(request, game)
+
+            serializer = GameSerializer(data=request.data)
+            if serializer.is_valid():
+                game.title = serializer.validated_data['title']
+                game.description = serializer.validated_data['description']
+                game.designer = serializer.validated_data['designer']
+                game.release_year = serializer.validated_data['release_year']
+                game.number_players = serializer.validated_data['number_players']
+                game.play_time = serializer.validated_data['play_time']
+                game.age_rec = serializer.validated_data['age_rec']
+                game.save()
+
+                serializer = GameSerializer(game, context={'request': request})
+                return Response(None, status.HTTP_204_NO_CONTENT)
+            
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        
         except Game.DoesNotExist:
-            return Response({'message': 'The game you are trying to edit does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
     def destroy(self, request, pk):
         pass
@@ -98,6 +92,14 @@ class CategorySerializer(serializers.ModelSerializer):
 class GameSerializer(serializers.ModelSerializer):
     categories = CategorySerializer(many=True, read_only=True)
 
+    # Declare that an ad-hoc property should be included in JSON
+    is_owner = serializers.SerializerMethodField()
+
+    # Function containing instructions for ad-hoc property
+    def get_is_owner(self, obj):
+        # Check if the authenticated user is the owner
+        return self.context['request'].user == obj.user
+
     class Meta:
         model = Game
-        fields = ('id', 'title', 'description', 'designer', 'release_year', 'number_players', 'play_time', 'age_rec', 'categories', )
+        fields = ('id', 'title', 'description', 'designer', 'release_year', 'number_players', 'play_time', 'age_rec', 'is_owner', 'categories', )
